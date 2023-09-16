22d3e04aabe97c6d11a8c04756a28e77d47592e2
gregPerlinLi <lihaolin13@outlook.com>
2023-09-16 23:04:04 +0800

    fix(settings.py):禁用 CSRF

--- cmdb_manager/middleware.py
+++ cmdb_manager/middleware.py
@@ -1,3 +1,10 @@
-class DisableCSRF(object):
+# tools/middleware.py
+
+from django.utils.deprecation import MiddlewareMixin
+
+class CloseCsrfMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        setattr(request, '_dont_enforce_csrf_checks', True)
+        request.csrf_processing_done = True  
+# class DisableCSRF(object):
+#     def process_request(self, request):
+#         setattr(request, '_dont_enforce_csrf_checks', True)
